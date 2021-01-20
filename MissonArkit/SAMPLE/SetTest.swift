224fe53c16364c2f4a12a55a0ff6fd9664ca929d
kenshihoo <redwing.ironrange@gmail.com>
2021-01-21 01:11:49 +0900

    gitinit

---
+++ MissonArkit/SAMPLE/SetTest.swift
@@ -0,0 +1,67 @@
+//
+//  SetTest.swift
+//  MissonArkit
+//
+//  Created by Kenshiro on 2021/01/20.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+import ARKit
+
+class SetTest: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        sceneView.delegate = self
+
+        sceneView.scene = SCNScene()
+    }
+    
+    //画面が呼ばれる直前の処理
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 特徴点を表示(なくてもいい)
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        // ライト追加
+        sceneView.autoenablesDefaultLighting = true;
+        
+        //空間認識をするARWorldTrackingConfigurationをインスタンス化
+        let configuration = ARWorldTrackingConfiguration()
+        //平面の検出を有効化
+        configuration.planeDetection = [.horizontal]
+        //ARセッションを開始
+        sceneView.session.run(configuration)
+    }
+
+    //viewの表示が終了(アプリが閉じられたり)した場合の処理
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        //ARセッションを停止
+        sceneView.session.pause()
+        
+    }
+    
+    
+//タップした最初の5つの座標を取得してprintする
+var fingers = [String?](repeating: nil, count:5)
+
+override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for touch in touches{
+        let point = touch.location(in: self.view)
+        for (index,finger)  in fingers.enumerated() {
+            if finger == nil {
+                fingers[index] = String(format: "%p", touch)
+                print("finger \(index+1): x=\(point.x) , y=\(point.y)")
+                break
+            }
+        }
+    }
+}
+}
--- MissonArkit/ViewController.swift
+++ MissonArkit/ViewController.swift
@@ -10,6 +10,9 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    var tapSceen:[CGPoint] = []
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -47,7 +50,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
         //上記まででARSCNViewの設定と平面検出ができるようになっている//
 
-    var tapcount =  0
+    
     
     //画面がタップされた場合の処理
     override func touchesBegan(_ touches: Set<UITouch>, with event:UIEvent?) {
@@ -55,13 +58,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //複数タッチの受信を可能にする
         sceneView.isMultipleTouchEnabled = true
         
-            //引数touchesはset型で保存されているらしい
-            //すべてのタッチ座標をprintする
-            for touch in touches{
-                let location = touch.location(in: sceneView)
-                print(location)
-            }
-
+        //タップされた位置の座標を保存する
+        for touch in touches{
+            
+            let point = touch.location(in: self.view)
+                tapSceen.append(point)
+        }
+        
         // 最初にタップした座標を取り出す
         guard let firstTouch = touches.first else {return}
         
@@ -79,10 +82,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             sceneView.session.add(anchor: anchor)
             }
         
-        //hitTestが現在非推奨のメソッドのため、raycastQuerを使うべきだが、変数anchorの設定がわからず挫折…
-        //let results = [sceneView.raycastQuery(from: touchPos,allowing: .existingPlane,alignment: .any)]
-        //let anchor = ARAnchor(transform: results.first??.accessibilityActivationPoint))
-        //sceneView.session.add(anchor: anchor)
+        
     }
     
     // シーンにARAnchorが追加されたときの処理
@@ -97,100 +97,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // 検出面の子要素にする
     node.addChildNode(sphereNode)
     }
+    
+    //オブジェクトが重なったらセッションを停止する(離れている位置が3cmいない)
+    func overlap() {
+        let firstTap = tapSceen.first
+        let lastTap = tapSceen.last
+        
+        if firstTap?.x == lastTap?.x{
+            if firstTap?.y == lastTap?.y {
+                //ARセッションを停止
+                sceneView.session.pause()
+            }
+        }
+    }
 }
-
-
-
-
-
-//
-//    var tapCount = 0
-//    var tapPos = CGPoint(x: 0, y: 0)
-//    var startPos = float3(0,0,0)
-//    var currentPos = float3(0,0,0)
-//
-//    // 球を描画するメソッド
-//    func putSphere(at pos: float3) {
-//
-//    let node = SCNNode()
-//
-//    //geometryを作成（半径と配置位置を設定）
-//    node.geometry = SCNSphere(radius:0.003)
-//    node.position = SCNVector3(pos.x, pos.y, pos.z)
-//
-//    self.sceneView.scene.rootNode.addChildNode(node)
-//    }
-//
-//    // 直線を描画するメソッド
-//    func drawLine(from : SCNVector3, to : SCNVector3) -> SCNNode{
-//        // 直線のGeometryを作成
-//        let source = SCNGeometrySource(vertices: [from, to])
-//        let element = SCNGeometryElement(data: Data.init(bytes: [0, 1]),primitiveType: .line,primitiveCount:1,bytesPerIndex: 1)
-//        let geometry = SCNGeometry(sources: [source], elements:[element])
-//
-//        // 直線ノードの作成
-//        let node = SCNNode()
-//
-//        node.geometry = geometry
-//        node.geometry?.materials.first?.diffuse.contents = UIColor.white
-//
-//        return node
-//    }
-//
-//
-//    //画面がタップされたときの処理
-//    override func touchesBegan(_ touches: Set<UITouch>, with event:UIEvent?) {
-//
-//        // タップが1回目の場合
-//        if tapCount == 0{
-//
-//            tapCount = +1
-//
-//            // 最初にタップした座標を取り出す
-//            guard let firsTouch = touches.first else {return}
-//
-//            // タップした座標をスクリーン座標に変換する
-//            let touchPos = firsTouch.location(in: sceneView)
-//            tapPos = touchPos
-//
-//            // タップされた位置のARアンカーを探す
-//            let hitTest = sceneView.hitTest(touchPos, types:.existingPlaneUsingExtent)
-//                if !hitTest.isEmpty {
-//
-//                //タップした箇所が取得できていればアンカーを追加
-//                let anchor = ARAnchor(transform: hitTest.first!.worldTransform)
-//
-//                //シーンにARAnchorを追加
-//                sceneView.session.add(anchor: anchor)
-//
-//                //startPosにアンカーを代入
-//                startPos = anchor as! float3
-//
-//                // タップされた位置を取得する（面との当たり判定）
-//                let hitResults = sceneView.hitTest(tapPos, types:[.existingPlaneUsingExtent])
-//
-//                // 結果取得に成功しているかどうか
-//                if !hitResults.isEmpty {
-//                    if let hitTResult = hitResults.first {
-//
-//                        // 実世界の座標をSCNVector3で返す
-//                        currentPos = float3(hitTResult.worldTransform.columns.3.x,hitTResult.worldTransform.columns.3.y,hitTResult.worldTransform.columns.3.z)}
-//
-//                // 球の配置
-//                putSphere(at:currentPos)
-//                }
-//            }
-//
-//        //2回目以降のタップの場合
-//        else{tapCount = +1
-//
-//                // 球の配置
-//                putSphere(at:currentPos)
-//
-//                //線状のオブジェクトを配置（1度目のタップと2度目のタップの間に配置）
-//                let lineNode = drawLine(from:SCNVector3(startPos),to:SCNVector3(currentPos))
-//
-//                sceneView.scene.rootNode.addChildNode(lineNode)
-//            }
-//        }
-//    }
